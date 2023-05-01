47316a1a7da13f319c417758f64fbdebbe1d3506
Marko Jelenkovic <marko92y@gmail.com>
2023-05-01 08:41:10 +0200

    suspence

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,27 +1,31 @@
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment, PresentationControls } from "@react-three/drei";
 import { Cup } from "./Cup";
+import { Loader } from "@react-three/drei";
 
 export const App = () => {
   return (
-    <Canvas dpr={[1, 2]} eventSource={document.getElementById("root")}>
-      <ambientLight intensity={0.5} />
-      <spotLight
-        intensity={0.5}
-        angle={0.1}
-        penumbra={1}
-        position={[0, 15, 10]}
-        castShadow
-      />
-      <Environment preset="city" />
-      <PresentationControls
-        speed={1.5}
-        global
-        polar={[-0.1, Math.PI / 4]}
-        rotation={[Math.PI / 10, 0, 0]}
-      >
-        <Cup position={[0, 1, 0]} />
-      </PresentationControls>
-    </Canvas>
+    <Suspense fallback={<Loader />}>
+      <Canvas dpr={[1, 2]} eventSource={document.getElementById("root")}>
+        <ambientLight intensity={0.5} />
+        <spotLight
+          intensity={0.5}
+          angle={0.1}
+          penumbra={1}
+          position={[0, 15, 10]}
+          castShadow
+        />
+        <Environment preset="city" />
+        <PresentationControls
+          speed={1.5}
+          global
+          polar={[-0.1, Math.PI / 4]}
+          rotation={[Math.PI / 10, 0, 0]}
+        >
+          <Cup position={[0, 1, 0]} />
+        </PresentationControls>
+      </Canvas>
+    </Suspense>
   );
 };
