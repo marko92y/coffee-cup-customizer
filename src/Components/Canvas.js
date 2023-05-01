eff4ccf6a558b747f7fec368be50637e4e02ffbc
Marko Jelenkovic <marko92y@gmail.com>
2023-05-01 10:58:08 +0200

    suspence

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,31 +1,27 @@
-import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment, PresentationControls } from "@react-three/drei";
 import { Cup } from "./Cup";
-import { Loader } from "@react-three/drei";
 
 export const App = () => {
   return (
-    <Suspense fallback={<Loader />}>
-      <Canvas dpr={[1, 2]} eventSource={document.getElementById("root")}>
-        <ambientLight intensity={0.5} />
-        <spotLight
-          intensity={0.1}
-          angle={1.1}
-          penumbra={1}
-          position={[0, 15, 10]}
-          castShadow
-        />
-        <Environment preset="city" />
-        <PresentationControls
-          speed={1.5}
-          global
-          polar={[-0.1, Math.PI / 4]}
-          rotation={[Math.PI / 10, 0, 0]}
-        >
-          <Cup position={[0, 1, 0]} />
-        </PresentationControls>
-      </Canvas>
-    </Suspense>
+    <Canvas dpr={[1, 2]} eventSource={document.getElementById("root")}>
+      <ambientLight intensity={0.5} />
+      <spotLight
+        intensity={0.1}
+        angle={1.1}
+        penumbra={1}
+        position={[0, 15, 10]}
+        castShadow
+      />
+      <Environment preset="city" />
+      <PresentationControls
+        speed={1.5}
+        global
+        polar={[-0.1, Math.PI / 4]}
+        rotation={[Math.PI / 10, 0, 0]}
+      >
+        <Cup position={[0, 1, 0]} />
+      </PresentationControls>
+    </Canvas>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Overlay from "./Components/Overlay";
 import { App as Canvas } from "./Components/Canvas";
-import reportWebVitals from "./reportWebVitals";
+import { Loader } from "@react-three/drei";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Canvas />
-    <Overlay />
+    <Suspense fallback={<Loader />}>
+      <Canvas />
+      <Overlay />
+    </Suspense>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
